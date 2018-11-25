ece6e81c911a5479a0f01ff4af369c6c2e3ffda9
Dramsammy <dramsammy@stuy.edu>
2018-11-24 21:12:18 -0500

    finished constructor and length method

--- MyString.java
+++ MyString.java
@@ -1,7 +1,15 @@
 public class MyString implements CharSequence,Comparable<CharSequence>{
   private char[] data;
+  // Constructor which takes in CharSequence datatype and takes length and creates array
   public MyString(CharSequence s){
-    s = data;
+    data = new char[s.length];
+    for (int index = 0; index < data.length; index++){
+      data[index] = s.charAt(index);
+    }
   }
-  public 
+  // Method that returns length --- uses array length
+  public int length(){
+    return data.length;
+  }
+
   }
